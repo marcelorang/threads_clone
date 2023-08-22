e7c622a863d2b2f1dbcb2e30872c9c2dc0aa4afc
marcelorang <mclowebdev@gmail.com>
2023-08-22 19:02:50 -0300

    feat: rendenring comments list in view

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -37,9 +37,26 @@ const Page = async ({ params }: { params: { id: string } }) => {
             <div className="mt-7">
                 <Comment
                     threadId={thread.id}
-                    currentUserImg={user.imageUrl}
+                    currentUserImg={userInfo.image}
                     currentUserId={JSON.stringify(userInfo._id)}
                 />
+            </div>
+
+            <div className="mt-10">
+                {thread.children.map((childItem: any) => (
+                    <ThreadCard
+                        id={childItem._id}
+                        key={childItem._id}
+                        currentUserId={childItem?.id || ""}
+                        parentId={childItem.parentId}
+                        content={childItem.text}
+                        author={childItem.author}
+                        community={childItem.community}
+                        createdAt={childItem.createdAt}
+                        comments={childItem.children}
+                        isComment
+                    />
+                ))}
             </div>
         </section>
     )
