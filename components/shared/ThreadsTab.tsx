ad58a092a8a8d6e82066ffa1845c75a06e6ade43
marcelorang <mclowebdev@gmail.com>
2023-08-22 21:42:41 -0300

    feat: fetch user Posts

---
+++ components/shared/ThreadsTab.tsx
@@ -0,0 +1,18 @@
+interface Props {
+    currentUserId: string;
+    accountId: string;
+    accountType: string;
+}
+
+
+const ThreadsTab = async function ({ currentUserId, accountId, accountType }: Props) {
+    // TODO:Fetch profile threads
+
+    return (
+        <section>
+            ThreadsTab
+        </section>
+    )
+}
+
+export default ThreadsTab;
